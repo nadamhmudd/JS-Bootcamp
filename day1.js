475491a8b5d1da27f5e9e8b4d7e2170a90f1e42a
Nada Mahmoud <nada.elroo799@gmail.com>
2022-07-16 11:58:10 +0200

    first commit

---
+++ day1.js
@@ -0,0 +1,31 @@
+//task 2 : Create Element
+document.write("<h1>JS Day 1</h1>")
+document.querySelector("h1").style.color = "blue";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.weight = "bold";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontFamily = "Arial";
+
+//task 3 : style directive
+console.log("%cJS %cBootcamp %c2021","color:red; font-size:40px", "color:green; font-size:40px", "color:white; background-color:blue; font-size:40px")
+
+//task 4
+console.group("Group 1");
+    console.log("Message one");
+    console.log("Message two");
+    console.group("Child Group");
+        console.log("Message one");
+        console.log("Message two");
+        console.group("Grand Child Group");
+            console.log("Message one");
+            console.log("Message two");
+        console.groupEnd();
+    console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+    console.log("Message one");
+    console.log("Message two");
+console.groupEnd();
+
+//task 5
+console.table(["Nada","Ahmed","Sameh","Gamal","Aya"]);
